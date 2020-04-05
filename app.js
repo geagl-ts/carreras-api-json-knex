0a09a4d80a11317e7aad2eaab52f26e7127a32ec
geagl-ts <miguel.toledo@itistmo.edu.mx>
2020-04-05 10:05:03 -0500

    quite el punto y coma del json de error

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.json({
     message: err.message,
-    error: req.app.get("env") === "development" ? err : {};
+    error: req.app.get("env") === "development" ? err : {},
   });
 });
 
